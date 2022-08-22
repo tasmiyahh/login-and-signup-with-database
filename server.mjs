9e82e4ffb97dbd98491e2a2f1875867488f603b2
tasmiyahh <tasmiyahhussainn99@gmail.com>
2022-08-23 01:33:18 +0500

    changes

--- server.mjs
+++ server.mjs
@@ -2,47 +2,35 @@ import express from 'express';
 import cors from 'cors';
 import { nanoid } from 'nanoid';
 import mongoose from 'mongoose';
-
-let dbURI = 'mongodb+srv://tasmiyah:web123@cluster0.cj82tmo.mongodb.net/socialmediabase?retryWrites=true&w=majority';
-
-mongoose.connect(dbURI);
-
-////////////////mongodb connected disconnected events///////////////////////////////////////////////
-mongoose.connection.on('connected', function() {//connected
-    console.log("Mongoose is connected");
-});
-
-mongoose.connection.on('disconnected', function() {//disconnected
-    console.log("Mongoose is disconnected");
-    process.exit(1);
-});
-
-mongoose.connection.on('error', function(err) {//any error
-    console.log('Mongoose connection error: ', err);
-    process.exit(1);
-});
-
-process.on('SIGINT', function() {/////this function will run jst before app is closing
-    console.log("app is terminating");
-    mongoose.connection.close(function() {
-        console.log('Mongoose default connection closed');
-        process.exit(0);
-    });
-});
-////////////////mongodb connected disconnected events///////////////////////////////////////////////
-
+import {
+    stringToHash,
+    varifyHash,
+    validateHash
+} from "bcrypt-inzi"
 
 
 
 const app = express()
-
-
 const port = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(cors());
 
 
+const userSchema = new mongoose.Schema({
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String, required: true },
+    password: { type: String },
+
+    age: { type: Number, min: 17, max: 65 },  //age ink darmiyan hui tw theek wrna save nh re ga
+    martialstatus: { type: Boolean, default: false },
+    
+    createdOn: { type: Date, default: Date.now },
+});
+const userModel = mongoose.model('user', userSchema);
+
+//there is no need of id as moongo provide id 
 
 app.post("/signup", (req, res) => {
 
@@ -53,7 +41,7 @@ app.post("/signup", (req, res) => {
         || !body.email
         || !body.password
     ) {
-        res.status(400).send(
+        res.status(404).send(
             `required fields missing, request example: 
                 {
                     "firstName": "John",
@@ -65,96 +53,143 @@ app.post("/signup", (req, res) => {
         return;
     }
 
-    let isFound = false;
+     // check if user already exist // query email user
+     userModel.findOne({ email: body.email }, (err, data) => {
+        if (!err) {
+            console.log("data: ", data);
 
-    for (let i = 0; i < userBase.length; i++) {
-        if (userBase[i].email === body.email.toLowerCase()) {
-            isFound = true;
-            break;
+            if (data) { // user already exist
+                console.log("user already exist: ", data);
+                res.status(400).send({ message: "user already exist,, please try a different email" });
+                return;
+
+            } else { // user not already exist
+
+                stringToHash(body.password).then(hashString => {
+
+                    let newUser = new userModel({
+                        firstName: body.firstName,
+                        lastName: body.lastName,
+                        email: body.email.toLowerCase(),
+                        password: hashString
+                    });
+                    newUser.save((err, result) => {
+                        if (!err) {
+                            console.log("data saved: ", result);
+                            res.status(201).send({ message: "user is created" });
+                        } else {
+                            console.log("db error: ", err);
+                            res.status(500).send({ message: "internal server error" });
+                        }
+                    });
+                })
+
+            }
+        } else {
+            console.log("db error: ", err);
+            res.status(500).send({ message: "db error in query" });
         }
-    }
-    if (isFound) { // this email already exist
-        res.status(400).send({
-            message: `email ${body.email} already exist.`
-        });
-        return;
-    }
-
-
-    let newUser = {
-        userId: nanoid(),
-        firstName: body.firstName,
-        lastName: body.lastName,
-        email: body.email.toLowerCase(),
-        password: body.password
-    }
-
-    userBase.push(newUser);
-
-    res.status(201).send({ message: "user is created" });
+    })
 });
 
-app.post("/login", (req, res) => {
+ 
 
-    let body = req.body;
 
-    if (!body.email || !body.password) {
-        res.status(400).send(
-            `required fields missing, request example: 
+
+
+
+
+
+        app.post("/login", (req, res) => {
+
+            let body = req.body;
+
+            if (!body.email || !body.password) {
+                res.status(404).send(
+                    `required fields missing, request example: 
                 {
                     "email": "abc@abc.com",
                     "password": "12345"
                 }`
-        );
-        return;
-    }
-
-    let isFound = false; // https://stackoverflow.com/a/17402180/4378475
-
-    for (let i = 0; i < userBase.length; i++) {
-        if (userBase[i].email === body.email) {
-
-            isFound = true;
-            if (userBase[i].password === body.password) { // correct password
-
-                res.status(200).send({
-                    firstName: userBase[i].firstName,
-                    lastName: userBase[i].lastName,
-                    email: userBase[i].email,
-                    message: "login successful",
-                    token: "some unique token"
-                })
+                );
                 return;
+            }
 
-            } else { // password incorrect
+            let isFound = false;
 
-                res.status(401).send({
-                    message: "incorrect password"
+            for (let i = 0; i < userBase.length; i++) {
+                if (userBase[i].email === body.email) {
+
+                    isFound = true;
+                    if (userBase[i].password === body.password) { // correct password
+
+                        res.status(200).send({
+                            firstName: userBase[i].firstName,
+                            lastName: userBase[i].lastName,
+                            email: userBase[i].email,
+                            message: "login successful",
+                            token: "some unique token"
+                        })
+                        return;
+
+                    } else { // password incorrect
+
+                        res.status(401).send({
+                            message: "incorrect password"
+                        })
+                        return;
+                    }
+                }
+            }
+
+
+
+
+            if (!isFound) {
+                res.status(404).send({
+                    message: "user not found"
                 })
                 return;
             }
-        }
-    }
-
-
-
-
-    if (!isFound) {
-        res.status(404).send({
-            message: "user not found"
         })
-        return;
-    }
-})
 
 
 
 
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+
+
+
+        let dbURI = 'mongodb+srv://tasmiyah:web@cluster0.cj82tmo.mongodb.net/socialmediabase?retryWrites=true&w=majority';
+        mongoose.connect(dbURI);
+
+        ////////////////mongodb connected disconnected events///////////////////////////////////////////////
+        mongoose.connection.on('connected', function () {//connected
+            console.log("Mongoose is connected");
+        });
+
+        mongoose.connection.on('disconnected', function () {//disconnected
+            console.log("Mongoose is disconnected");
+            process.exit(1);
+        });
+
+        mongoose.connection.on('error', function (err) {//any error
+            console.log('Mongoose connection error: ', err);
+            process.exit(1);
+        });
+
+        process.on('SIGINT', function () {/////this function will run jst before app is closing
+            console.log("app is terminating");
+            mongoose.connection.close(function () {
+                console.log('Mongoose default connection closed');
+                process.exit(0);
+            });
+        });
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
 
 
 
